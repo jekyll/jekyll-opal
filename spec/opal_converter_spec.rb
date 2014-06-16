664c37eac9e6eb8b2d01a5c6e48d25066d61c5e5
Parker Moore <parkrmoore@gmail.com>
2014-06-16 00:26:56 -0400

    Add some tests.
    Closes #1.

---
+++ spec/opal_converter_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe(Jekyll::Converters::Opal) do
+
+  it "converts Ruby into Opal"
+
+  it "matches .rb files"
+
+  it "outputs .js"
+
+  it "explodes on bad input"
+
+end
